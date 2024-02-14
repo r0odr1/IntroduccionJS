ceebc4ce6e3038dd3b08a3847b4b1ac2e878c89c
Rodrigo <rhbon.08@gmail.com>
2024-02-14 11:13:37 -0500

    forma antigua de declarar variables

---
+++ js/01.js
@@ -0,0 +1,19 @@
+// Variables
+
+var producto = 'Audifonos'; //Iniciar variable y asignar valor
+
+var disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // PascalCase
+var nombreproducto = 'Monitor HD';
