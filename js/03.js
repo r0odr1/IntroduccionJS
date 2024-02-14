90735ed25aff09b6d8000a2084725937a565bb73
Rodrigo <rhbon.08@gmail.com>
2024-02-14 11:18:58 -0500

    declarar variables con const

--- js/03.js
+++ js/03.js
@@ -1,10 +1,8 @@
 const producto = 'Audifonos'; //Iniciar variable y asignar valor
 
-const disponible; // Iniciamos la variable pero sin valor
+const disponible = true // Iniciamos la variable pero tiene que tener valor
 
 producto = true; // Reasignamos el valor de la variable
-
-disponible = true;
 
 const producto1 = 'Computadora',
     disponible1 = true,
