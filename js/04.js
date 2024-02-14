238951952925f664d43eb6ff9b68c991bce995b1
Rodrigo <rhbon.08@gmail.com>
2024-02-14 11:33:36 -0500

    algunos metodos para strings

---
+++ js/04.js
@@ -0,0 +1,18 @@
+// String o cadenas de texto
+
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo";
+const producto2 = "Monitor HD";
+const email = 'correo@correo.com';
+
+// length es para la extension
+console.log(tweet.length);
+console.log(producto2);
+
+// IndexOf (Retorna posicion)
+console.log(tweet.indexOf("JavaScript"));
+console.log(producto2.indexOf("Tablet"));
+console.log(email.indexOf("@"));
+
+// Include (retorna true o false)
+console.log(tweet.includes("JavaScript"));
+console.log(producto2.includes("Tablet"));
