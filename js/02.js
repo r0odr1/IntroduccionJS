cef56c4799d63117a86a0ef43031e59ce0e576e4
Rodrigo <rhbon.08@gmail.com>
2024-02-14 11:17:22 -0500

    declarar variables con let

---
+++ js/02.js
@@ -0,0 +1,19 @@
+// Variables con let
+
+let producto = 'Audifonos'; //Iniciar variable y asignar valor
+
+let disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // Camelcase
+let NombreProducto = 'Monitor HD'; // PascalCase
+let nombreproducto = 'Monitor HD';
---
+++ js/03.js
@@ -0,0 +1,17 @@
+const producto = 'Audifonos'; //Iniciar variable y asignar valor
+
+const disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // PascalCase
+const nombreproducto = 'Monitor HD';
