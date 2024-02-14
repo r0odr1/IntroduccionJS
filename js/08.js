c3dd5bbb8a57f46a1d71382e44188c5bdb6b20cc
Rodrigo <rhbon.08@gmail.com>
2024-02-14 15:02:37 -0500

    Concatenacion y template string

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = "Rodrigo";
+const email = "correo@correo.com";
+
+// concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+console.log("Nombre Cliente:", nombre, "Email:", email);
+
+//template String - String Literals
+console.log(`Nombre Cliente: ${nombre} Email ${email}`);
