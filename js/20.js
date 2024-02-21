52851d63572af6ad1299bf280969dc563e0a2192
Rodrigo <rhbon.08@gmail.com>
2024-02-21 18:32:43 -0500

    Metodos de propiedad

---
+++ js/20.js
@@ -0,0 +1,25 @@
+// Metodos de rpopiedad
+const reproductor = {
+  reproducir: function (id) {
+    console.log(`Reproduciendo Cancion con el ID: ${id}`);
+  },
+  pausar: function () {
+    console.log("Pausando...");
+  },
+  crearPlaylist: function (nombre) {
+    console.log(`Creando la playlist: ${nombre}`);
+  },
+  reproducirPlaylist: function (nombre) {
+    console.log(`Reproduciendo la playlist: ${nombre}`);
+  },
+};
+
+reproductor.borrarCancion = function (id) {
+  console.log(`Eliminando la cancion: ${id}`);
+};
+
+reproductor.reproducir(3840);
+reproductor.pausar();
+reproductor.borrarCancion(20);
+reproductor.crearPlaylist("Salsa");
+reproductor.reproducirPlaylist("Salsa");
