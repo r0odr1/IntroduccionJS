36ee0747f31b356c5f273d00646d9554ca18d67b
Rodrigo <rhbon.08@gmail.com>
2024-02-29 18:28:52 -0500

    for loops

---
+++ js/24.js
@@ -0,0 +1,31 @@
+// For Loop
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`El numero: ${i} es par`);
+//   }else {
+//     console.log(`El numero: ${i} es impar`);
+//   }
+// }
+
+const carrito = [
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Televisión 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
+];
+
+for (let i = 0; i < carrito.length; i++) {
+  console.log(carrito[i].nombre);
+}
+
+//While Loop
+
+// Do While Loop
