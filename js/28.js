18f4565bc558b3c53be54e8c0fa321bb25442bad
Rodrigo <rhbon.08@gmail.com>
2024-03-01 12:57:12 -0500

    POO: Herencia

--- js/28.js
+++ js/28.js
@@ -6,7 +6,7 @@ class Producto {
     this.precio = precio;
   }
   formatearProducto() {
-    return `El  Producto ${this.nombre} tiene un preciode : ${this.precio}`;
+    return `El  Producto ${this.nombre} tiene un precio de: ${this.precio}`;
   }
   formatearPrecio() {
     return `El  Precio es de: ${this.precio}`;
@@ -15,6 +15,21 @@ class Producto {
 
 const prducto2 = new Producto("Monitor Curvo de 49", 800);
 const prducto3 = new Producto("Laptop", 300);
+
+// Herencia
+
+class Libro extends Producto {
+  constructor(nombre, precio, isbn) {
+    super(nombre, precio);
+    this.isbn = isbn;
+  }
+  formatearProducto() {
+    return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+  }
+}
+
+const libro = new Libro("Javascript la Revolución", 120, "910321554");
+console.log(libro.formatearProducto());
 
 console.log(prducto2);
 console.log(prducto3);
